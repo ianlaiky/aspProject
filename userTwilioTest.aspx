cc6ea3086ffe249a53734cdf39c98fe72c3e40ef
Ian <152772a@mymail.nyp.edu.sg>
2017-07-03 22:22:08 +0800

    tw

---
+++ userTwilioTest.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/register.master" AutoEventWireup="true" CodeFile="userTwilioTest.aspx.cs" Inherits="userTwilioTest" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    
+    
+    
+    
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_OnClick"/>
+</asp:Content>
+
---
+++ userTwilioTest.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+public partial class userTwilioTest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_OnClick(object sender, EventArgs e)
+    {
+
+
+
+
+        // Your Account SID from twilio.com/console
+        var accountSid = "ACa1126441199f69f7fe3d08c38004da41";
+        // Your Auth Token from twilio.com/console
+        var authToken = "e3fdbb9a5b96a5f170be0b455cd4ef8a";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = MessageResource.Create(
+            to: new PhoneNumber("+6598517761"),
+            from: new PhoneNumber("+16304746289 "),
+            body: "Hello from C#");
+
+        Console.WriteLine(message.Sid);
+        Console.Write("Press any key to continue.");
+        Console.ReadKey();
+
+
+
+    }
+}
