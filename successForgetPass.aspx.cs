b8eb7dfb3eb3949b8ebb42dbe1af0184ca01bd95
Aravin <aravinmanokaran10@gmail.com>
2017-07-24 21:23:15 +0800

    password change page

--- successForgetPass.aspx.cs
+++ successForgetPass.aspx.cs
@@ -11,4 +11,9 @@ public partial class successForgetPass : System.Web.UI.Page
     {
 
     }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+      Response.Redirect("login.aspx");
+    }
 }
