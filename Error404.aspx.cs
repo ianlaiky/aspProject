0e84490416e26320812a46e80bdf839f0a1662b4
Ian <152772a@mymail.nyp.edu.sg>
2017-08-03 12:47:15 +0800

    update

---
+++ Error404.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Error404 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_OnClick(object sender, EventArgs e)
+    {
+        Response.Redirect("login.aspx");
+    }
+}
